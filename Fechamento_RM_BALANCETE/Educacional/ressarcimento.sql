ff26f46815811342ec363774bd7166b7504f0569
Cesar Lopes <cesargl@sebraesp.com.br>
2024-05-17 14:15:59 -0300

    UPDATE: Educacional

--- Fechamento_RM_BALANCETE/Educacional/ressarcimento.sql
+++ Fechamento_RM_BALANCETE/Educacional/ressarcimento.sql
@@ -1,36 +1,36 @@
 
   DECLARE @dt date = getdate();
-
-  WITH TMOV AS (
-    SELECT *
-    FROM CORPORERM.TMOV
-        WHERE CODTMV LIKE '1.2.17'
-        AND YEAR(DATAEMISSAO) = YEAR(@DT)
-        AND [STATUS] NOT IN ('C', 'R')
+  use HubDados;
+    WITH TMOV AS (
+      SELECT *
+      FROM CORPORERM.TMOV
+          WHERE CODTMV LIKE '1.2.17'
+          AND YEAR(DATAEMISSAO) = YEAR(@DT)
+          AND [STATUS] NOT IN ('C', 'R')
+          )
+        ,
+        TVEN AS (
+          SELECT CODVEN,NOME FROM CorporeRM.TVEN
         )
-  ,
-  TVEN AS (
-    SELECT CODVEN,NOME FROM CorporeRM.TVEN
-  )
-  ,
-  ITM AS (
-    SELECT a.*
-    ,C.NOMEFANTASIA
-    ,D.NOME AS VEN1
-    ,B.CODVEN2
-    ,B.Historico
-    ,B.[STATUS] AS ST_MOV
-    ,B.DATACRIACAO
-      FROM CorporeRM.TITMMOV A
-      INNER JOIN TMOV B
-      ON A.IDMOV = B.IDMOV
-      INNER JOIN CorporeRM.TPRD c
-      ON A.IDPRD = C.IDPRD
-      LEFT JOIN TVEN D
-      ON B.CODVEN1 = D.CODVEN
-      
+        ,
+        ITM AS (
+          SELECT a.*
+          ,C.NOMEFANTASIA
+          ,D.NOME AS VEN1
+          ,B.CODVEN2
+          ,B.Historico
+          ,B.[STATUS] AS ST_MOV
+          ,B.DATACRIACAO
+            FROM CorporeRM.TITMMOV A
+            INNER JOIN TMOV B
+            ON A.IDMOV = B.IDMOV
+            INNER JOIN CorporeRM.TPRD c
+            ON A.IDPRD = C.IDPRD
+            LEFT JOIN TVEN D
+            ON B.CODVEN1 = D.CODVEN
+            
 
-  )
+        )
 
 
-  SELECT * FROM ITM WHERE NOMEFANTASIA NOT IN ('VIAGENS NACIONAIS - FUNCIONÁRIOS', 'SERVIÇO DE FRETE E CARRETO - FUNCIONÁRIOS')
+    SELECT * FROM ITM WHERE NOMEFANTASIA NOT IN ('VIAGENS NACIONAIS - FUNCIONÁRIOS', 'SERVIÇO DE FRETE E CARRETO - FUNCIONÁRIOS')
